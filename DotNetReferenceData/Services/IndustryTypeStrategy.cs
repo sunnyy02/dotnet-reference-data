70786aa018ab027b56a20614c12997508449519e
sunny <sunnyy@gmail.com>
2024-09-22 16:00:58 +1000

    wip

---
+++ DotNetReferenceData/Services/IndustryTypeStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DotNetReferenceData.repo.Interface;
+using DotNetReferenceData.Services.Interface;
+
+namespace DotNetReferenceData.Services;
+
+public class IndustryTypeStrategy : IRefdataStrategy
+{
+    private readonly IRepositoryBase<IndustryDataDto> repo;
+	public IndustryTypeStrategy(IRepositoryBase<IndustryDataDto> repo)
+	{
+        this.repo = repo;
+	}
+
+    public async Task<IEnumerable<RefData>> GetRefData()
+    {
+        return await this.repo.GetAll();
+    }
+}
+
---
+++ DotNetReferenceData/repo/Interface/IRepositoryBase.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace DotNetReferenceData.repo.Interface;
+
+public interface IRepositoryBase<TEntity> where TEntity : class
+{
+    Task<IEnumerable<TEntity>> GetAll();
+}
+
